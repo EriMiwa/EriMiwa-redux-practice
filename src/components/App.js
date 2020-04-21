cf0d14e26135beebc889afdbf1aaadc2d5078483
Erika Miwa <erikamiwa612@gmail.com>
2020-04-21 15:59:58 -0700

    add reducer

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AddTodo from '../containers/AddTodo'
+
+const App = () => {
+  return (
+    <div className="App">
+      <AddTodo/>
+    </div>
+  );
+}
+
+export default App;
